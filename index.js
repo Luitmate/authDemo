87306d96645a389e2dc9c63472bc8a4819d40784
lunov08 <lukitmate@gmail.com>
2021-09-11 11:33:39 -0300

    crea db y lo conecta con registro

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const path = require('path');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+//------SET MONGOOSE------//
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/authDemo');
+}
+
+//------SET EJS------//
 app.set('view engine', 'ejs');
-app.set('views', 'views'),
+app.set('views', path.join(__dirname, 'views')),
+
+//------PARSER------//
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+    res.send('HOME PAGE');
+})
 
 app.get('/registro', (req, res) => {
     res.render('registro');
 })
 
+app.post('/registro', async (req, res) => {
+    const {username, password} = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username, 
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
+})
 
 app.get('/secret', (req, res) => {
     res.send('Esta es una página secreta solo para usuarios logueados');
